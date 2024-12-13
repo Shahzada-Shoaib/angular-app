ad8ee1341d2b90113d89e7eee6171f66d6903698
shahzada-shoaib <shahzada.shoaib011@gmail.com>
2024-12-13 05:09:08 +0500

    ui of form

--- Angular-job-projects/practice-app/test-app/src/app/app.routes.ts
+++ Angular-job-projects/practice-app/test-app/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { GetApiComponent } from './get-api/get-api.component';
+import { FormsComponent } from './forms/forms.component';
 
 export const routes: Routes = [
     {
         path: 'get-api',
          component: GetApiComponent
+    },
+    {
+        path: 'forms',
+        component: FormsComponent
     }
+
 ];
